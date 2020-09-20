9d8bdc33f40d131a307b2b559f6ea4a67afe9baa
GuyFawkess <skakunostap@gmail.com>
2020-09-20 10:03:15 +0300

    -1000 -1

--- dz_for_19.09/dz.py
+++ dz_for_19.09/dz.py
@@ -1,3 +1,3 @@
-a = [i+1 for i in range(1000)]
-a = [i ** 2 - 2 * len(str(i)) for i in a if sum([int(j) for j in str(i)]) >= i and i // 10 % 10 != 7]
+a = [i+1 for i in range(-1000, -1)]
+a = [i ** 2 - 2 * len(str(i)) for i in a if sum([int(j) for j in str(i)[1:]]) >= abs(i) and i // 10 % 10 != -7]
 print(a)
