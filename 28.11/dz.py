b5f988a75f1f106bcd24b3db714887d08e38b05d
GuyFawkess <skakunostap@gmail.com>
2020-12-05 09:35:25 +0200

    4.12

--- 28.11/dz.py
+++ 28.11/dz.py
@@ -53,37 +53,37 @@ class Fighter(AbstractFighter):
     def __init__(self, name: str, damage: int, hp: int, strength: int, agility: int):
         if not isinstance(name, str):
             raise TypeError('attribute \'name\' must be string!')
-        self._name = name
+        self.__name = name
         if not isinstance(hp, int) or hp <= 0:
             raise TypeError('attribute \'damage\' must be positive integer!')
-        self._damage = damage
+        self.__damage = damage
         if not isinstance(hp, int) or hp <= 0:
             raise TypeError('attribute \'hp\' must be positive integer!')
-        self._hp = hp
+        self.__hp = hp
         self.__max_hp = hp
         if not isinstance(hp, int) or hp <= 0:
             raise TypeError('attribute \'strength\' must be positive integer!')
-        self._strength = strength
+        self.__strength = strength
         if not isinstance(hp, int) or hp <= 0:
             raise TypeError('attribute \'agility\' must be positive integer!')
-        self._agility = agility
-        self._wins = 0
-        self._losses = 0
+        self.__agility = agility
+        self.__wins = 0
+        self.__losses = 0
 
     def get_name(self):
-        return self._name
+        return self.__name
 
     def get_damage(self):
-        return self._damage
+        return self.__damage
 
     def get_health(self):
-        return self._hp
+        return self.__hp
 
     def get_strength(self):
-        return self._strength
+        return self.__strength
 
     def get_agility(self):
-        return self._agility
+        return self.__agility
 
     def attack(self, fighter):
         if int(random.random() * 100) <= 100 - (fighter.get_strength() + fighter.get_agility()):
@@ -92,36 +92,36 @@ class Fighter(AbstractFighter):
         return False
 
     def print_combat_history(self):
-        print(f'Name: {self.get_name()}, Wins: {self._wins}, Losses: {self._losses}')
+        print(f'Name: {self.get_name()}, Wins: {self.__wins}, Losses: {self.__losses}')
         return
 
     def heal(self, hp: int):
-        self._hp += hp
-        if self._hp > self.__max_hp:
-            self._hp = self.__max_hp
+        self.__hp += hp
+        if self.__hp > self.__max_hp:
+            self.__hp = self.__max_hp
         return
 
     def deal_damage(self, hp: int):
-        self._hp -= hp
-        if self._hp <= 0:
-            self._hp = 0
+        self.__hp -= hp
+        if self.__hp <= 0:
+            self.__hp = 0
         return
 
     def add_win(self):
-        self._wins += 1
+        self.__wins += 1
         return
 
     def add_loss(self):
-        self._losses += 1
+        self.__losses += 1
         return
 
 
 def battle(fighter1: Fighter, fighter2: Fighter):
     if fighter1.get_health() == 0:
-        print(f'{fighter1.get_name()} is dead and can\' fight.')
+        print(f'{fighter1.get_name()} is dead and can\'t fight.')
         return
     if fighter2.get_health() == 0:
-        print(f'{fighter2.get_name()} is dead and can\' fight.')
+        print(f'{fighter2.get_name()} is dead and can\'t fight.')
         return
     fighters = (fighter1, fighter2)
     ch = random.randint(0, 1)
@@ -133,6 +133,7 @@ def battle(fighter1: Fighter, fighter2: Fighter):
             )
         else:
             print(f'{fighters[ch].get_name()} attack missed')
+        print(f'{fighter1.get_name()} hp: {fighter1.get_health()}; {fighter2.get_name()} hp: {fighter2.get_health()}')
         ch += 1
         ch %= 2
     if fighter1.get_health() == 0:
@@ -147,6 +148,6 @@ def battle(fighter1: Fighter, fighter2: Fighter):
 
 
 fighter1 = Fighter('Maximus', 20, 100, 20, 15)
-fighter2 = Fighter('Commodus', 25, 90, 25, 10)
+fighter2 = Fighter('Commodus', 25, 90, 25, 20)
 
 battle(fighter1, fighter2)
